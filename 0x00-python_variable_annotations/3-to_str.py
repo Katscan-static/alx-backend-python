307b9cafb7159775a3f09107dfd47d73766ee90d
Katekani Madingana <madinganakn@gmail.com>
2023-12-07 11:59:39 +0200

    3 to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    - This module converts a float number (n) into a string 
+    - Returns a string type value of n
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        to_str:
+            converts value (n) float to string type
+
+        Args:
+            n (float): palceholder for a float type number
+
+        Returns:
+            str: string type representation of (n)
+    """
+
+    return str(n)
