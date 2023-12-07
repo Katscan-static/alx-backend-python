b7488d66c3db12e8b768cff8b170b0b34cdba01f
Katekani Madingana <madinganakn@gmail.com>
2023-12-07 11:40:39 +0200

    concat using annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    - This module concatenates two strings.
+    - Returns concatenated string
+    - annotaions: str1 and str2 are strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        concat:
+            concatincates two strings
+
+        Args:
+            str1 (str): first string placeholder
+            str2 (str): second string placeholder
+
+        Returns:
+            str: returns concatenated string
+    """
+
+    return str1 + str2
