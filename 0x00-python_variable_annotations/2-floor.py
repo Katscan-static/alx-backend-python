65d3ea2b3c3dbdc42aba51031a3cff797c493bd5
Katekani Madingana <madinganakn@gmail.com>
2023-12-07 11:49:10 +0200

    2 floor a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    - This module returns the floor of a float number (n)
+"""
+
+
+def floor(n: float) -> int:
+    """
+        floor:
+            converts float to the floor value
+
+        Args:
+            n (float): float number placeholder
+
+        Returns:
+            int: the floor value of n
+    """
+
+    return int(n)
