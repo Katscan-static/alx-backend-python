ba6d006f15339613a4fc37334dfb0e3e01519b88
Katekani Madingana <madinganakn@gmail.com>
2023-12-07 11:32:20 +0200

    add python annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    a type-annotated function takes a and b as float and returns a sum float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        add:
+            function adds two float numbers
+
+        Args:
+            a (float): a is a placeholder
+            b (float): b is a placeholder
+
+        Returns:
+            float: sum of a and b
+    """
+    return a + b
