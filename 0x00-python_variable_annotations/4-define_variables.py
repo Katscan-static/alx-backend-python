74e903129be3c0407bc1f087c4188f12672a8986
Katekani Madingana <madinganakn@gmail.com>
2023-12-07 12:14:20 +0200

    4 define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    -This module annotates and initialises variables with a value
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
