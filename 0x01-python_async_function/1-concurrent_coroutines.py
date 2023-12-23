3791009647f3ac9d4305a50697b3142a6befa39a
Katekani Madingana <madinganakn@gmail.com>
2023-12-23 12:22:41 +0200

    basic async syntax fixed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -22,6 +22,6 @@ async def wait_n(n: int, max_delay: int) -> list:
     delays = []
     tasks = [wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
-        delayi = await task
+        delay = await task
         delays.append(delay)
     return delays
